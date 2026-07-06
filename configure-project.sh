ef39cf721463d0e58378020f93aed46c6e419a12
juan294949 <juan2949@yahoo.com>
2026-07-06 14:02:02 -0500

    updating repo

--- configure-project.sh
+++ configure-project.sh
@@ -6,38 +6,36 @@ echo "Staring configuration."
 PWD_PATH=$(pwd)
 PATH_TO_LAYERS="${PWD}/layers"
 BRANCH="scarthgap"
-layers_list=()
-repos_list=()
+declare -a repos_list
+declare -a layers_list
 
-git clone -b "${BRANCH}" "https://github.com/juan294949/poky.git"
-git clone -b "${BRANCH}" "https://github.com/juan294949/meta-zybo.git"
+git clone -b "${BRANCH}" "https://github.com/juan294949/poky.git" "${PATH_TO_LAYERS}/poky"
+git clone -b "${BRANCH}" "https://github.com/juan294949/meta-zybo.git" "${PATH_TO_LAYERS}/meta-zybo"
 
 echo "Configuring environment:"
-source ${PWD_PATH}/oecore/oe-init-build-env
+source ${PATH_TO_LAYERS}/poky/oe-init-build-env
 
 # Repositories needed for the project.
-while IFS= read -r line
-  do
-    repos_list+=$line
+while IFS= read -r line; do
+    repos_list+=("$line")
 done < ${PWD_PATH}/configuration/repos_list.txt
 
-echo "Downloading repos needed for the project."
 # Download the repositories needed and change to the branch required.
 # Currently using scarthgap.
-
+echo "Downloading repos needed for the project."
 for repo in "${repos_list[@]}"; do
   if [[ -d ${PATH_TO_LAYERS}/${repo} ]]; then
     echo "repository found at: ${PATH_TO_LAYERS}/${repo}"
   else
-    echo " clonning: ${repo} from: ${repo}"
-    git clone -b "${BRANCH}" "${repo}" "${PATH_TO_LAYERS}/${repo}"
+    echo " clonning: ${repo}"
+    git clone -b "${BRANCH}" "${repo}" "${PATH_TO_LAYERS}/$(basename "$repo" .git)"
   fi
 done
 
 # Layers to add in the bblayer.conf file.
 while IFS= read -r line
   do
-    bitbake-layers add-layer "${PATH_TO_LAYERS}/${repo}/${line}"
+    bitbake-layers add-layer "${PATH_TO_LAYERS}/${line}"
 done < ${PWD_PATH}/configuration/layers_list.txt
 
 cd $PWD_PATH
